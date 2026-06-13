be643bd2de4800105c77e2572c6d61205fdb7a43
r0ttenbeef <captain_midnight1337@protonmail.ch>
2026-06-13 13:27:53 +0000

    Update site URL in Astro configuration to point to r0ttenbeef.github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://apx-linux.github.io',
+	site: 'https://r0ttenbeef.github.io',
 	base: '/apx-homepage',
 	integrations: [mdx(), sitemap()],
 });
